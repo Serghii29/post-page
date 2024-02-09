6897f47baf7feb8aff7ebe4f18e7d7d2faa221e2
Serhii Tretyak <tretyak8936@gmail.com>
2024-02-09 17:33:17 +0200

    added an error handler and loading

--- src/PostsList/PostsList.tsx
+++ src/PostsList/PostsList.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Loader } from 'semantic-ui-react';
 import { useGetPostsQuery } from '../services/posts';
 import { PostItem } from '../PostItem';
 import './PostList.scss';
 
 export const PostsList: React.FC = () => {
-  const { data } = useGetPostsQuery();
+  const { data, isError, isLoading } = useGetPostsQuery();
+
+  if (isError) {
+    return (
+      <div>
+        Error: something went wrong(((
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <Loader>Loading...</Loader>
+    );
+  }
 
   return (
     <div className="list-container">
       {data?.map((post) => (
-        <div className="item-container">
-          <PostItem key={post.id} post={post} />
+        <div className="item-container" key={post.id}>
+          <PostItem post={post} />
         </div>
       ))}
     </div>
